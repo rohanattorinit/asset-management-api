a9f9ba78d846e6a971038562a1284ee0ed86bbf2
akramansaritorinit <akram.ansari@torinit.ca>
2022-08-25 01:27:47 -0700

    files added

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const knex = require("knex");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(4000, () => {
+  console.log("App is running in port: 4000");
+});
