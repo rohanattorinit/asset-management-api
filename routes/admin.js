fc2e88ac4e4e3014dfdc1c0f48a3a049d2c00dbb
akramansaritorinit <akram.ansari@torinit.ca>
2022-08-30 11:43:06 +0530

    employee route and admin route added

--- routes/admin.js
+++ routes/admin.js
@@ -5,65 +5,53 @@ const bcrypt = require("bcrypt-nodejs");
 
 //register new employee
 router.post("/addEmployee", async (req, res) => {
-   const { empId, name, email, phone, gender, location, jobTitle } = req.body;
-   const hash = bcrypt.hashSync(email);
-   db("employees")
-      .insert({
-         empId,
-         name,
-         email,
-         password: hash,
-         phone,
-         gender,
-         location,
-         jobTitle,
-      })
-      .then(() => {
-         res.status(200).json({ message: "Resgistration Successfull!" });
-      })
-      .catch((error) => res.status(400).json({ error }));
-});
-
-//chnage password
-router.post("/changePassword/:id", async (req, res) => {
-   const { password } = req.body;
-   const hash = bcrypt.hashSync(password);
-   db("employees")
-      .where("empId", req.params.id)
-      .update({ password: hash })
-      .then(() => {
-         res.status(200).json({ message: "Password Changed Successfully!" });
-      });
+  const { empId, name, email, phone, gender, location, jobTitle } = req.body;
+  const hash = bcrypt.hashSync(email);
+  db("employees")
+    .insert({
+      empId,
+      name,
+      email,
+      password: hash,
+      phone,
+      gender,
+      location,
+      jobTitle,
+    })
+    .then(() => {
+      res.status(200).json({ message: "Resgistration Successfull!" });
+    })
+    .catch((error) => res.status(400).json({ error }));
 });
 
 //delete an employee
 router.delete("/:id", async (req, res) => {
-   db("employees")
-      .where("empId", req.params.id)
-      .del()
-      .then((data) => {
-         res.status(200).json({ message: "Employee Deleted successfully" });
-      })
-      .catch((error) => {
-         res.status(400).json({ error });
-      });
+  db("employees")
+    .where("empId", req.params.id)
+    .del()
+    .then((data) => {
+      res.status(200).json({ message: "Employee Deleted successfully" });
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 });
 
 //login
 router.post("/login", async (req, res) => {
-   const { email, password } = req.body;
-   db.select("email", "password")
-      .from("employees")
-      .where("email", "=", email)
-      .then((data) => {
-         const isValid = bcrypt.compareSync(password, data[0].password);
-         if (isValid) {
-            res.status(200).json({ message: "Login Successfull" });
-         } else {
-            res.status(400).json({ error: "Wrong credintials!" });
-         }
-      })
-      .catch((error) => res.status(400).json({ error: "User not found!" }));
+  const { email, password } = req.body;
+  db.select("email", "password")
+    .from("employees")
+    .where("email", "=", email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].password);
+      if (isValid) {
+        res.status(200).json({ message: "Login Successfull" });
+      } else {
+        res.status(400).json({ error: "Wrong credintials!" });
+      }
+    })
+    .catch((error) => res.status(400).json({ error: "User not found!" }));
 });
 
 module.exports = router;
--- routes/employees.js
+++ routes/employees.js
@@ -4,51 +4,67 @@ const { db } = require("../config/connection");
 
 //get all employees
 router.get("/", async (req, res) => {
-   db.select("*")
-      .from("employees")
-      .then((data) => {
-         res.status(200).json({
-            message: "All employees fetched successfully",
-            data: data,
-         });
-      })
-      .catch((error) => {
-         res.status(400).json({ error });
+  db.select("*")
+    .from("employees")
+    .then((data) => {
+      res.status(200).json({
+        message: "All employees fetched successfully",
+        data: data,
       });
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 });
 
 //get a single employee
-router.get("/(:id)", async (req, res) => {
-   const id = req.params.id;
-   db.select("*")
-      .from("employees")
-      .where("EmpId", id)
-      .then((data) => {
-         res.status(200).json({
-            message: "Employee fetched successfully",
-            data: data,
-         });
-      })
-      .catch((error) => {
-         res.status(400).json({ error });
-      });
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("employees")
+    .where("empId", "=", id)
+    .then((data) => {
+      if (data[0]) {
+        res.status(200).json({ data });
+      } else {
+        res.status(400).json({ error: "Employee not found!" });
+      }
+    })
+    .catch((error) => res.status(400).json({ error }));
 });
 
 //update an employee
-router.post("/update/(:id)", async (req, res) => {
-   const empData = req.body;
-   const id = empData.id;
-   db("employees")
-      .insert(empData)
-      .then((data) => {
-         res.status(200).json({
-            message: "Employee created successfully",
-            data,
-         });
-      })
-      .catch((error) => {
-         res.status(400).json({ error });
-      });
+router.post("/update/:id", async (req, res) => {
+  const { name, email, phone, gender, location, jobTitle } = req.body;
+  const { id } = req.params;
+  db("employees")
+    .where("empId", id)
+    .update({
+      name,
+      email,
+      phone,
+      gender,
+      location,
+      jobTitle,
+    })
+    .then(() => {
+      res.status(200).json({ message: "Details Updated successfully" });
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+});
+
+//chnage password
+router.post("/changePassword/:id", async (req, res) => {
+  const { password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  db("employees")
+    .where("empId", req.params.id)
+    .update({ password: hash })
+    .then(() => {
+      res.status(200).json({ message: "Password Changed Successfully!" });
+    });
 });
 
 module.exports = router;
