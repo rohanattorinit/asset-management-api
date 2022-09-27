eca9a1813e6f217da027cf4317d8c3ffed52aa87
Rohan Desai <111341927+rohanattorinit@users.noreply.github.com>
2022-09-28 03:01:21 +0530

    asset csv added

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,3 +1,4 @@
+import { isAuth, isAdmin } from "./../middleware/authorization";
 import express, { Request, Response } from "express";
 const router = express.Router();
 import moment from "moment";
@@ -5,29 +6,36 @@ import db from "../config/connection";
 import bcrypt from "bcrypt";
 
 //allocate an asset to an employee
-router.post("/allocateAsset/:empId", async (req: Request, res: Response) => {
-  const { empId } = req.params;
-  const { assetId } = req.body;
-  const assetallocation = assetId?.map((asset: number) => {
-    return { empId, assetId: asset };
-  });
-  assetallocation.map((asset: { empId: string; assetId: number }) => {
-    db("assets")
-      .update({ status: "allocated" })
-      .where("assetId", asset.assetId)
+router.post(
+  "/allocateAsset/:empId",
+  isAuth,
+  isAdmin,
+  async (req: Request, res: Response) => {
+    const { empId } = req.params;
+    const { assetId } = req.body;
+    const assetallocation = assetId?.map((asset: number) => {
+      return { empId, assetId: asset };
+    });
+    assetallocation.map((asset: { empId: string; assetId: number }) => {
+      db("assets")
+        .update({ status: "allocated" })
+        .where("assetId", asset.assetId)
+        .catch((error) => res.status(400).json({ error }));
+    });
+    db("assetallocation")
+      .insert(assetallocation)
+      .then(() => {
+        res.status(200).json({ message: "Asset allocated Successfully!" });
+      })
       .catch((error) => res.status(400).json({ error }));
-  });
-  db("assetallocation")
-    .insert(assetallocation)
-    .then(() => {
-      res.status(200).json({ message: "Asset allocated Successfully!" });
-    })
-    .catch((error) => res.status(400).json({ error }));
-});
+  }
+);
 
 //deallocate an asset
 router.post(
   "/deallocateAsset/:empId/:assetId",
+  isAuth,
+  isAdmin,
   async (req: Request, res: Response) => {
     const { empId, assetId } = req.params;
     db("assetallocation")
