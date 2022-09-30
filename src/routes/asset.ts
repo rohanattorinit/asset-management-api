6a111b3ca1881d25cf81f3431912a477f3544574
Rohan Desai <111341927+rohanattorinit@users.noreply.github.com>
2022-09-30 18:12:55 +0530

    Asset changes

--- src/routes/asset.ts
+++ src/routes/asset.ts
@@ -9,7 +9,6 @@ import csv from "csv-parser";
 const upload = multer({ dest: "uploads/" });
 
 interface Asset {
-
   assetId?: number;
   brandId: number;
   name: string;
@@ -58,157 +57,157 @@ router.get("/", isAuth, isAdmin, async (req, res: Response) => {
 });
 
 //get a single asset
-router.get
+router.get(
   "/:assetId",
   isAuth,
   isAdmin,
   async (req: Request, res: Response) => {
     const { assetId } = req.params;
     db<Asset>("assets")
-
       .select("*")
       .then((data) => {
-         res.status(200).json({
-            message: "All assets fetched successfully",
-            data: data,
-         });
+        res.status(200).json({
+          message: "All assets fetched successfully",
+          data: data,
+        });
       })
       .catch((error) => {
-         res.status(400).json({ error });
+        res.status(400).json({ error });
       });
-});
+  }
+);
 
 //get a single asset
 router.get(
-   "/:assetId",
-   isAuth,
-   isAdmin,
-   async (req: Request, res: Response) => {
-      const { assetId } = req.params;
-      db<Asset>("assets")
-         .select("*")
-         .join("brands", "assets.brandId", "=", "brands.brandid")
-         .where("assetId", "=", assetId)
-         .then((data) => {
-            res.status(200).json({
-               message: `Asset ${assetId} fetched successfully`,
-               data: data,
-            });
-         })
-         .catch((error) => {
-            res.status(400).json({ error });
-         });
-   }
+  "/:assetId",
+  isAuth,
+  isAdmin,
+  async (req: Request, res: Response) => {
+    const { assetId } = req.params;
+    db<Asset>("assets")
+      .select("*")
+      .join("brands", "assets.brandId", "=", "brands.brandid")
+      .where("assetId", "=", assetId)
+      .then((data) => {
+        res.status(200).json({
+          message: `Asset ${assetId} fetched successfully`,
+          data: data,
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({ error });
+      });
+  }
 );
 
 //get all assets of a single employee
 router.get("/employeeAssets/:empId", isAuth, async (req, res) => {
-
-   const { empId } = req.params;
-   db.select(
-      "assets.assetId",
-      "brands.name",
-      "assets.modelno",
-      "assets.category",
-      "assetallocation.allocationTime"
-   )
-      .from("assetallocation")
-      .join("assets", "assetallocation.assetId", "=", "assets.assetId")
-      .join("brands", "assets.brandId", "=", "brands.brandid")
-      .where("assetallocation.empId", empId)
-      .then((data) => {
-         res.status(200).json({
-            message: `All assets fetched for employee: ${empId} successfully`,
-            data: data,
-         });
-      })
-      .catch((error) => {
-         res.status(400).json({ error });
-
+  const { empId } = req.params;
+  db.select(
+    "assets.assetId",
+    "brands.name",
+    "assets.modelno",
+    "assets.category",
+    "assetallocation.allocationTime"
+  )
+    .from("assetallocation")
+    .join("assets", "assetallocation.assetId", "=", "assets.assetId")
+    .join("brands", "assets.brandId", "=", "brands.brandid")
+    .where("assetallocation.empId", empId)
+    .then((data) => {
+      res.status(200).json({
+        message: `All assets fetched for employee: ${empId} successfully`,
+        data: data,
       });
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
 });
 
 //create a new asset
 router.post("/addAsset", isAuth, isAdmin, async (req, res) => {
+  try {
+    const {
+      assetName,
+      brandName,
+      assetType,
+      category,
+      modelNo,
+      description,
+      status,
+      usability,
+      isRented,
+      asset_location,
+      vendor,
+      rent,
+      deposit,
+      rentStartDate,
+      rentEndDate,
+    } = req.body;
 
-   try {
-      const {
-         assetName,
-         brandName,
-         assetType,
-         category,
-         modelNo,
-         description,
-         status,
-         usability,
-         isRented,
-         vendor,
-         rent,
-         deposit,
-         rentStartDate,
-         rentEndDate,
-      } = req.body;
-
-      if (isRented) {
-         if (!vendor || !deposit || !rentStartDate || !rentEndDate) {
-            return res
-               .status(400)
-               .json({ error: "Please provide rental details!" });
-         }
+    if (isRented) {
+      if (!vendor || !deposit || !rentStartDate || !rentEndDate) {
+        return res
+          .status(400)
+          .json({ error: "Please provide rental details!" });
       }
+    }
 
-      //find brand Id using the brand name given in request body
-      const brandArr = await db("brands")
-         .select("brandId")
-         .where("name", "=", brandName);
-      const brandId = brandArr[0].brandId;
+    //find brand Id using the brand name given in request body
+    const brandArr = await db("brands")
+      .select("brandId")
+      .where("name", "=", brandName);
+    const brandId = brandArr[0].brandId;
 
-      const asset: Asset = isRented
-         ? {
-              brandId,
-              name: assetName,
-              assetType,
-              category,
-              modelNo,
-              description,
-              status,
-              usability,
-              addedTime: moment().format("YYYY-MM-DD HH:mm:ss"),
-              isRented,
-              vendor,
-              rent,
-              deposit,
-              rentStartDate,
-              rentEndDate,
-           }
-         : {
-              brandId,
-              name: assetName,
-              assetType,
-              category,
-              modelNo,
-              description,
-              status,
-              usability,
-              addedTime: moment().format("YYYY-MM-DD HH:mm:ss"),
-           };
+    const asset: Asset = isRented
+      ? {
+          brandId,
+          name: assetName,
+          assetType,
+          category,
+          modelNo,
+          description,
+          status,
+          usability,
+          asset_location,
+          addedTime: moment().format("YYYY-MM-DD HH:mm:ss"),
+          isRented,
+          vendor,
+          rent,
+          deposit,
+          rentStartDate,
+          rentEndDate,
+        }
+      : {
+          brandId,
+          name: assetName,
+          assetType,
+          category,
+          modelNo,
+          description,
+          status,
+          usability,
+          asset_location,
+          addedTime: moment().format("YYYY-MM-DD HH:mm:ss"),
+        };
 
-      db<Asset>("assets")
-         .insert(asset)
-         .then(() =>
-            res.status(200).json({
-               message: "Asset created successfully",
-            })
-         )
-         .catch((error) =>
-            res.status(400).json({
-               error: "Error occured while creating asset",
-               errorMsg: error,
-            })
-         );
-   } catch (error) {
-      res.status(400).json({ error });
-   }
+    db<Asset>("assets")
+      .insert(asset)
+      .then(() =>
+        res.status(200).json({
+          message: "Asset created successfully",
+        })
+      )
+      .catch((error) =>
+        res.status(400).json({
+          error: "Error occured while creating asset",
+          errorMsg: error,
+        })
+      );
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 router.post(
