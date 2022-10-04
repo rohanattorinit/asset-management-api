68d283e4edc1e3c0a28a8569ceefcbac8793f94e
Rohan Desai <111341927+rohanattorinit@users.noreply.github.com>
2022-10-04 18:24:26 +0530

    serverless function

--- src/index.ts
+++ src/index.ts
@@ -27,5 +27,5 @@ app.use("/api/brands", brandsRoute);
 app.use("/api/assets", assetRoute);
 app.use("/api/tickets", ticketRoute);
 
-// module.exports.handler = serverless(app);
-app.listen(4000, () => console.log("Server started"));
+module.exports.handler = serverless(app);
+// app.listen(4000, () => console.log("Server started"));
