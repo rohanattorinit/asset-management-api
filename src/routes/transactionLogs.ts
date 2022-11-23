f2db0b7ef4f94244e00d3254866fa17b158b654d
Arch1198 <108786876+Arch1198@users.noreply.github.com>
2022-11-23 16:28:55 +0530

    changes in transactionLog.ts regarding transaction api

--- src/routes/transactionLogs.ts
+++ src/routes/transactionLogs.ts
@@ -12,23 +12,7 @@ router.get(
         res.status(400).json({error: "Asset Id is missing!"})
       }
       try {
-        // Get data from asset_update table and store the array in a constant 
-        // const asset_updation_logs = await db('asset_update').select("asset_update_id","updated_feature","description","effective_date as updated_at")
-        //                         .where('asset_id',assetId)
-                                
-        // const ticket_logs = await db('tickets').join('employees','employees.empId','tickets.empId').select("employees.empId","employees.name as empName","tickets.ticketId","tickets.createdAt as created_at","tickets.closedAt as closed_at")
-        //                         .where('assetId',assetId)
-
-        // const allocation_logs = await db('transaction_history').select("emp_id","emp_name","allocation_date as allocated_at","deallocation_date as deallocated_at")
-        //                         .where('asset_id',assetId)
-        // const {received_at,deleted_at} = await db('assets').select("received_date as received_at","deleted_at").where('assetId',assetId).first()
-        // const data = {
-        //     asset_updation_logs: asset_updation_logs,
-        //     ticket_logs:ticket_logs,
-        //     allocation_logs:allocation_logs,
-        //     received_at:received_at,
-        //     deleted_at:deleted_at
-        // }
+        
         const data =await db("transaction_log").select("*").where("asset_id",assetId).orderBy('date');
         res.status(200).json({
             message: `Logs fetched successfully for assetId ${assetId}`,
@@ -44,7 +28,7 @@ router.get(
   // Get count for pie charts
 router.get("/categoryCount", async (req:Request, res: Response) => {
     try {
-      // console.log(req)
+      
       // get total count of all categories of assets
       const totalCount = await db.select('category').from('assets').count('* as count').groupBy('category');
       
